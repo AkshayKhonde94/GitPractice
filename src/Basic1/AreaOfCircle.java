0e822229dba4c0c40354461d4355d03f742f9d0f
akshaykhonde94 <akshay.khonde94@protonmail.com>
2021-10-31 23:46:37 +0530

    modified radius variable

--- src/Basic1/AreaOfCircle.java
+++ src/Basic1/AreaOfCircle.java
@@ -3,13 +3,13 @@ package Basic1;
 public class AreaOfCircle {
 
 	public static void main(String[] args) {
-		int radius = 7;
+		int radius_of_circle = 7;
 		
-		double area = Math.PI * radius * radius;
+		double area = Math.PI * radius_of_circle * radius_of_circle;
 		System.out.println(area);
 		
 		
-		double perimeter = 2 *Math.PI * radius;
+		double perimeter = 2 *Math.PI * radius_of_circle;
 		System.out.println(perimeter);
 	}
 
