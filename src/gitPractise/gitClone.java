865c81224635c5a8d45fea0f5422fc735cbd5486
akshaykhonde94 <akshay.khonde94@protonmail.com>
2021-11-01 23:10:07 +0530

    created gitPractise

---
+++ src/gitPractise/gitClone.java
@@ -0,0 +1,12 @@
+package gitPractise;
+
+public class gitClone {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Git Clone command");
+		
+		
+	}
+
+}
---
+++ src/gitPractise/gitStatus.java
@@ -0,0 +1,10 @@
+package gitPractise;
+
+public class gitStatus {
+
+	public static void main(String[] args) {
+		System.out.println("Git Status command");
+
+	}
+
+}
