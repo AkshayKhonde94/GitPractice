f708a1c8b99895293ce03eea0e16d1e61d2a47f2
akshaykhonde94 <akshay.khonde94@protonmail.com>
2021-11-01 23:12:24 +0530

    added gitlog and gitpush

---
+++ src/gitPractise/gitPush.java
@@ -0,0 +1,10 @@
+package gitPractise;
+
+public class gitPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Push commands");
+	}
+
+}
